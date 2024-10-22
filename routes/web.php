7122cf83101a2caf9b1ff8ea1e4bc3926794fc6b
balan081194 <balancivil9133@gmail.com>
2024-10-22 17:07:07 +0530

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
+use App\Http\Controllers\AdminController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/register', [AdminController::class, 'adminRegister']);
 
 
 Route::get('/index', function () {
