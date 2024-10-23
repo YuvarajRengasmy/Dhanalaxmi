a4eb2396514b4e74b602f9ad3070099c6a6835ab
JayJo17 <gopinath172023@gmail.com>
2024-10-23 18:11:50 +0530

    home

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/index', function () {
     return view('finovo.index');
 });
 
