7d5dc569836fbeece75ccda1263ed98223b89cfe
Yuvaraj R <119421277+YuvarajRengasmy@users.noreply.github.com>
2024-10-24 13:07:04 +0530

    init

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
-    public function contactDetails(Request $request){
-        dd($request->all());
+    public function viewContactForm(){
+        return view('finovo.contact');
+     }
+
+
+
+    public function registerContactForm(Request $request){
+        // dd($request->all());
+        Contact::create($request->all());
+        return redirect('/contact')->with('message', 'Register Successfully!');
     }
 }
